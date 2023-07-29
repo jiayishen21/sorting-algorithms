8d411bf22a139f4c653cb7001adbb8b31935a2dd
jiayishen21 <lucasshen21@gmail.com>
2023-07-29 23:45:12 +0800

    merge sort basic implementation complete, only thing left to do is to create comparing indices and setCodePosition appropriately

--- src/components/MergeSort.jsx
+++ src/components/MergeSort.jsx
@@ -10,6 +10,20 @@ export const MergeSort = () => {
 	const [speed, setSpeed] = useState(1);
 	const [waitTime, setWaitTime] = useState(baseWaitTime)
 
+	const flattenArray = (arr) => {
+		let flattenedArray = [];
+	
+		arr.forEach((element) => {
+			if (Array.isArray(element)) {
+				flattenedArray.push(...flattenArray(element));
+			} else {
+				flattenedArray.push(element);
+			}
+		});
+	
+		return flattenedArray;
+	}
+
 	const randomArray = (length) => {
 		const newArr = []
 		for (let i = 0; i < length; i++) {
@@ -104,14 +118,24 @@ export const MergeSort = () => {
 		await delay(50)
 		setCodePosition(0)
     setTimer(0)
+    setCurPath([])
+    setMerging(false)
+		const newArrs = flattenArray(arrs)
+		setArrs(newArrs)
 	}
 
 	const onClickSort = () => {
 		if(!sorting) {
       setTimer(0)
+			setCodePosition(0)
 			setSorting(true)
+      setCurPath([])
+      setMerging(false)
 		}
 	}
+
+  const [curPath, setCurPath] = useState([])
+  const [merging, setMerging] = useState(false)
 
 	useEffect(() => {
 		(async() => {
@@ -122,6 +146,131 @@ export const MergeSort = () => {
 			if(timer > 0) {
 				decreaseTimer()
 				return
+			}
+
+      let curArr = arrs
+      for(let i of curPath) {
+        curArr = curArr[i]
+      }
+
+      if(merging) {
+				setTimer(waitTime)
+				const newArrs = [...arrs]
+				curArr = newArrs
+				for(let i of curPath) {
+					curArr = curArr[i]
+				}
+
+				if(curArr.length < 3) {
+					curArr.unshift([])
+				}
+
+				if(curArr[1] && curArr[1].length === 0) {
+					const combined = curArr[0].concat(curArr[2])
+					curArr.splice(0)
+					curArr.push(...combined)
+
+					const newPath = [...curPath]
+
+					if(curPath.length === 0) {
+						setArrs(newArrs)
+						await onStop();
+						toast.success('Array sorted.')
+						return;
+					}
+
+					// If currently on right path
+					if(newPath[newPath.length - 1]) {
+						newPath.pop()
+					}
+					// If on left path, move to right path
+					else {
+						newPath[newPath.length - 1] = 1
+						setMerging(false)
+					}
+					setCurPath(newPath)
+				}
+				else if(curArr[2] && curArr[2].length === 0) {
+					const combined = curArr[0].concat(curArr[1])
+					curArr.splice(0)
+					curArr.push(...combined)
+
+					const newPath = [...curPath]
+
+					if(curPath.length === 0) {
+						setArrs(newArrs)
+						await onStop();
+						toast.success('Array sorted.')
+						return;
+					}
+
+					// If currently on right path	
+					if(newPath[newPath.length - 1]) {
+						newPath.pop()
+					}
+					// If on left path, move to right path
+					else {
+						newPath[newPath.length - 1] = 1
+						setMerging(false)
+					}
+
+					setCurPath(newPath)
+				}
+
+				else {
+					if(curArr[1][0] < curArr[2][0]) {
+						curArr[0].push(curArr[1].shift())
+					}
+					else {
+						curArr[0].push(curArr[2].shift())
+					}
+				}
+				setArrs(newArrs)
+				return
+      }
+
+      if(curArr.length <= 1) {
+        setTimer(waitTime)
+        // setCodePosition(1)
+
+        // If currently at right array
+        if(curPath[-1]) {
+					// Merge
+          const newCurPath = [...curPath]
+          newCurPath.pop()
+          setCurPath(newCurPath)
+          setMerging(true)
+        }
+        // If currentl at left array
+        else {
+					// Move into the right array
+          const newCurPath = [...curPath]
+          newCurPath[-1] = 1
+          setCurPath(newCurPath)
+        }
+        return
+      }
+			// If current array is larger than length 1, split
+			else {
+				setTimer(waitTime)
+				const middle = Math.floor(curArr.length / 2)
+				const leftHalf = curArr.slice(0, middle);
+ 				const rightHalf = curArr.slice(middle);
+
+				// Update arrs
+				const newArrs = [...arrs]
+				let newCurArr = newArrs
+				for(let i of curPath) {
+					newCurArr = newCurArr[i]
+				}
+				newCurArr.splice(0)
+				newCurArr.push(leftHalf)
+				newCurArr.push(rightHalf)
+				setArrs(newArrs)
+
+				const newCurPath = curPath
+				newCurPath.push(0)
+				setCurPath(newCurPath)
 			}
 
 		}) ()
@@ -161,27 +310,51 @@ export const MergeSort = () => {
 				</div>
 
 				<code>
-					{`for(let i = 0; i < arr.length - 1; i ++) {`} <br/>
-					&nbsp;&nbsp;{`let sorted = true`}	<br/>
-					<br/>
-					&nbsp;&nbsp;{`for(let j = 0; j < arr.length - 1 - i; j ++) {`}	<br/>
+					{`const mergeSort = (arr) => {`} <br/>
 					<div className={codePosition === 1 ? 'highlighted-code' : ''}>
-						&nbsp;&nbsp;&nbsp;&nbsp;{`if(arr[j] > arr[j + 1]) {`}	<br/>
+						&nbsp;&nbsp;{`if(arr.length <= 1) {`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`return arr`}	<br/>
+						&nbsp;&nbsp;{`if(arr.length <= 1) {`}	<br/>
 					</div>
+					<br/>
 					<div className={codePosition === 2 ? 'highlighted-code' : ''}>
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`sorted = false`}	<br/>
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`const temp = arr[j]`}	<br/>
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`arr[j] = arr[j + 1]`}	<br/>
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`arr[j + 1] = temp`}	<br/>
+						&nbsp;&nbsp;{`const middle = Math.floor(arr.length / 2)`}	<br/>
+						&nbsp;&nbsp;{`const leftHalf = arr.slice(0, middle)`}	<br/>
+						&nbsp;&nbsp;{`const rightHalf = arr.slice(middle)`}	<br/>
+						<br />
+						&nbsp;&nbsp;{`const sortedLeft = mergeSort(leftHalf)`}	<br/>
+						&nbsp;&nbsp;{`const sortedRight = mergeSort(rightHalf)`}	<br/>
+						<br />
+						&nbsp;&nbsp;{`return merge(sortedLeft, sortedRight)`}	<br/>
 					</div>
-					&nbsp;&nbsp;&nbsp;&nbsp;{`}`}	<br/>
-					&nbsp;&nbsp;{`}`}	<br/>
-					{`	`}	<br/>
+					{`}`} <br />
+					<br />
+					{`const merge = (arr) => {`} <br/>
+					&nbsp;&nbsp;{`let combined = []`}	<br/>
+					&nbsp;&nbsp;{`let leftIndex = 0`}	<br/>
+					&nbsp;&nbsp;{`let rightIndex = 0`}	<br/>
+					<br />
+					&nbsp;&nbsp;{`while(leftIndex < left.length && rightIndex < right.length) {`}	<br/>
 					<div className={codePosition === 3 ? 'highlighted-code' : ''}>
-						&nbsp;&nbsp;{`if(sorted) {`}	<br/>
-						&nbsp;&nbsp;&nbsp;&nbsp;{`break`}	<br/>
-						&nbsp;&nbsp;{`}`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`if(left[leftIndex] < right[rightIndex]) {`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`combined.push(left[leftIndex])`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`leftIndex++`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`}`}	<br/>
 					</div>
+					<br />
+					<div className={codePosition === 4 ? 'highlighted-code' : ''}>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`else {`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`combined.push(right[rightIndex])`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`rightIndex++`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`}`}	<br/>
+					</div>
+					&nbsp;&nbsp;{`}`}	<br/>
+					<br />
+					<div className={codePosition === 5 ? 'highlighted-code' : ''}>
+						&nbsp;&nbsp;{`return combined`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`.concat(left.slice(leftIndex))`}	<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;{`.concat(right.slice(rightIndex))`}	<br/>
+						</div>
 					{`}`}
 				</code>
 
